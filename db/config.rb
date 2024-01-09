20868c1e7cf52448df44590a35b7024d5909544d
Bas Vodde <bas.vodde@caci.nl>
2024-01-09 16:39:36 +0800

    Changed database config

--- db/config.rb
+++ db/config.rb
@@ -6,7 +6,8 @@ end
 
 configure :production do
   log "PRODUCTION"
-  DataMapper.setup(:default, ENV['REFACTORINGMANIFESTO_DB_URL'])
+#  DataMapper.setup(:default, ENV['REFACTORINGMANIFESTO_DB_URL'])
+  DataMapper.setup(:default, "postgres://127.0.0.1/refactoringmanifesto")
 end
 
 configure :test do
